2fa0e01a3d396d4161b8a233ad503584c1ccf103
MartinaTropeano <103671014+MartinaTropeano@users.noreply.github.com>
2022-04-14 19:12:45 +0200

    Creazione

---
+++ src/emotionalsongs/Persona.java
@@ -0,0 +1,81 @@
+package emotionalsongs;
+
+import java.io.Serializable;
+
+public class Persona implements Serializable, Comparable<Persona> {
+
+    //campi
+    private String nome;
+    private String cognome;
+    private String codiceFiscale;
+    private String indirizzoFisico;
+    private String email;
+    private String password;
+
+    //costruttore
+    public Persona(){}
+
+    public Persona(String nome, String cognome, String codiceFiscale, String indirizzoFisico, String email, String password){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.codiceFiscale = codiceFiscale;
+        this.indirizzoFisico = indirizzoFisico;
+        this.email = email;
+        this.password = password;
+    }
+
+    //getter
+    public String getNome(){
+        return nome;
+    }
+
+    public String getCognome(){
+        return cognome;
+    }
+
+    public String getCodiceFiscale(){
+        return codiceFiscale;
+    }
+
+    public String getIndirizzoFisico(){
+        return indirizzoFisico;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    //setter
+    public void setNome(String nuovoNome){
+        this.nome = nuovoNome;
+    }
+
+    public void setCognome(String nuovoCognome){
+        this.cognome = nuovoCognome;
+    }
+
+    public void setCodiceFiscale(String nuovoCodiceFiscale){
+        this.codiceFiscale = nuovoCodiceFiscale;
+    }
+
+    public void setIndirizzoFisico(String nuovoIndirizzoFisico){
+        this.indirizzoFisico = nuovoIndirizzoFisico;
+    }
+
+    public void setEmail(String nuovaEmail){
+        this.email = nuovaEmail;
+    }
+
+    public void setPassword(String nuovaPassword){
+        this.password = nuovaPassword;
+    }
+
+    public int compareTo(Persona persona) {
+        if(persona == null) return 1;
+        return codiceFiscale.compareTo(persona.codiceFiscale);
+    }
+}
---
+++ src/emotionalsongs/Persone.java
@@ -0,0 +1,26 @@
+package emotionalsongs;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Persone implements Serializable {
+
+    private List<Persona> listaPersone;
+    /*private InterfaceDB<Persona> DB;
+
+    public InterfaceDB<Persona> getDB() {
+        return DB;
+    }
+
+    public void setDB(InterfaceDB<Persona> DB) {
+        this.DB = DB;
+    }
+    */
+    public List<Persona> getListaPersone() {
+        return listaPersone;
+    }
+
+    public void setListaPersone(List<Persona> listaPersone) {
+        this.listaPersone = listaPersone;
+    }
+}
