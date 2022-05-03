df18e9f5d97bb4823adfb9cf30ba81d17c4e8d4b
PierpaoloGuenzani <pierpaolo.guenzani@gmail.com>
2022-05-03 10:30:59 +0200

    aggiunti NullPointerException

--- src/emotionalsongs/FileDB.java
+++ src/emotionalsongs/FileDB.java
@@ -16,6 +16,8 @@ public class FileDB<T> implements InterfaceDB<T>
 	
 	public FileDB(String percorsoFile) throws IOException
 	{
+		if(percorsoFile == null)
+			throw new NullPointerException("il percorso non puo' essere null");
 		path = Paths.get(percorsoFile);
 		file = path.toFile();
 		if (!file.exists())
@@ -27,14 +29,15 @@ public class FileDB<T> implements InterfaceDB<T>
 			input = new FileInputStream(file);
 			output = new FileOutputStream(file);
 		}
+		//else ecezzzione da creare...
 	}
 	
 	public List getAll() throws IOException, ClassNotFoundException
 	{
 		ObjectInputStream objectstream = new ObjectInputStream(input);
 		ArrayList<T> lista = new ArrayList<T>();
-		T t = (T) objectstream.readObject();  // casting di persona o canzone o playlist
-		while (t != null)
+		T t = (T) objectstream.readObject();	// casting di persona o canzone o playlist
+		while (t != null)						//leggo il file fino a quando non trovo un oggetto null
 		{
 			lista.add(t);
 			t = (T) objectstream.readObject();  // Rileggiamo il file con oggetti
@@ -45,15 +48,23 @@ public class FileDB<T> implements InterfaceDB<T>
 	
 	public Optional<T> get(Object id)
 	{
+		if(id == null)
+			throw new NullPointerException("il dato non puo' essere null");
 		return null;
 	}
 	
 	public void save(T t)
 	{
+		if(t == null)
+			throw new NullPointerException("il dato non puo' essere null");
 	}
 	
 	public void update(T base, T modificato)
 	{
+		if(base == null)
+			throw new NullPointerException("il dato da modificare non puo' essere null");
+		if(modificato == null)
+			throw new NullPointerException("il dato da sostituire non puo' essere null");
 	}
 	
 	public void delete(T t)
