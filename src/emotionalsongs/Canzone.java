04191b13b0c8190422d189d4bce2408abfb04d98
Ade2995 <paolocuvato95@gmail.com>
2022-05-05 18:22:06 +0200

    modifica Canzone,Canzoni

--- src/emotionalsongs/Canzone.java
+++ src/emotionalsongs/Canzone.java
@@ -2,52 +2,72 @@ package emotionalsongs;
 
 import java.io.Serializable;
 
-public class Canzone implements Serializable
+public class Canzone implements Serializable,Comparable<Canzone>
 {
 	private int ID;
 	private String Nome;
 	private String Artista;
-	
+	private  int anno;
+
 	public Canzone()
 	{
 	}
-	
-	public Canzone(int ID, String Nome, String Artista)
+
+	public Canzone(int ID, String Nome, String Artista,int anno)
 	{
-		
+
 		this.ID = ID;
 		this.Nome = Nome;
 		this.Artista = Artista;
-		
+		this.anno=anno;
+
 	}
-	
+
 	public int getID()
 	{
 		return ID;
 	}
-	
+
 	public void setID(int ID)
 	{
 		this.ID = ID;
 	}
-	
+
 	public String getNome()
 	{
 		return Nome;
 	}
-	
+
 	public void setNome(String nome)
 	{
 		this.Nome = nome;
 	}
-	
+
 	public String getArtista()
 	{
 		return Artista;
 	}
-	
+
 	public void setArtista(String Artista)
 	{
 		this.Artista = Artista;
 	}
+
+	public int getAnno() {
+		return anno;
+	}
+
+	public void setAnno(int anno) {
+		this.anno = anno;
+	}
+
+	@Override
+	public int compareTo(Canzone o) {
+		if (ID== o.getID())
+			return 0;
+		if (ID> o.getID()|| o== null)
+			return 1;
+		else  return -1;
+	}
 }
+// scrive compare to e restituisce  1 se maggiore ,-1 se minore e 0 se e uguale
--- src/emotionalsongs/Canzoni.java
+++ src/emotionalsongs/Canzoni.java
@@ -1,24 +1,61 @@
 package emotionalsongs;
 
-public class Canzoni
-{
-	
-	private int N;
-	Canzone[] InsiemeCanzoni = new Canzone[N];
-	
-	Canzoni(int N)
-	{
-		this.N = N;
-	}
-	
-	Canzoni()
-	{
-	}
-	
-	//CercaBrano(){
-	
-	//CercaPerArtista(){
-	
-	//CercaPerBrano(){
-	
+import java.lang.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Canzoni  {
+    private InterfaceDB<Canzone> db;
+    private List<Canzone> listacanzoni;
+
+    Canzoni() {
+        listacanzoni = db.getAll();
+    }
+
+    public List cercaBranoMusicale(String nomeBrano) {
+        LinkedList<Canzone> listaBrani= new LinkedList<Canzone>();
+
+        for (Canzone c: listacanzoni) {
+            if (nomeBrano.equals(c.getNome() ))
+                listaBrani.add(c);
+        }
+        return listaBrani;
+    }
+
+    public List cercaBranoMusicale(String artista,int anno){
+        LinkedList<Canzone> listaBrani= new LinkedList<Canzone>();
+        for (Canzone c : listacanzoni) {
+            if(artista.equals(c.getArtista())&& anno== c.getAnno()) {
+                listaBrani.add(c);
+            }
+        }
+        return  listaBrani;
+
+    }
+    public Canzone cercaBranoMusicale(int Id) {
+        for (Canzone c: listacanzoni) {
+            if (Id == c.getID())
+                return  c;
+        }
+        return null;
+    }
+
 }
+
+//CercaPerBrano(id){
+//if canzone.id == id
+// return canzone
+
+//CercaBrano(String nomebrano){
+//return canzone
+
+// return String="";
+// }
+//CercaPerArtista(artista){
+
+
+// cerchi artista nella lista se e presente aggiungi tutte canzoni a una lined e la restituisci
+// return Linked list
+//return List Canzone
