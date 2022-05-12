28b592f51ce1635e81cce59cce16ae267cafd2c6
Febby16 <fabijparadise@gmail.com>
2022-05-12 16:15:10 +0200

    Aggiunto commento in Percezione e in Emozione

--- src/emotionalsongs/Emozione.java
+++ src/emotionalsongs/Emozione.java
@@ -2,6 +2,11 @@ package emotionalsongs;
 
 import java.io.Serializable;
 
+/**
+ * Le sue istanze rappresentano le emozioni che possono essere selezionate dall'utente
+ * @author Paradiso Fabiola
+*/
+
 public enum Emozione implements Serializable
 {
     WONDER,
--- src/emotionalsongs/Percezione.java
+++ src/emotionalsongs/Percezione.java
@@ -1,5 +1,10 @@
 package emotionalsongs;
 
+/**
+ * Le sue istanze rappresentano tutto ciò che viene associato all'emozione/alle emozioni seleionate
+ * @author Paradiso Fabiola
+ */
+
 public class Percezione { 
     
     private final Emozione emozione;
@@ -7,6 +12,16 @@ public class Percezione {
     private final String note;
     private final String songId;
     private final String userId;
+
+    /**
+     * Costruisce un oggetto che contiene i dati associati all'emozione/alle emozioni selezionate
+     * @param emozione l'emozione selezionata
+     * @param score il punteggio che rappresenta il grado si intensità dell'emozione provata
+     * @param note il piccolo spazio di testo in cui poter specificare ulteriori dettagli riguardo all'emozione provata
+     * @param songId il nome della canzone associato all'emozione
+     * @param userId il nome utente che seleziona 'emozione provata
+     */
+    //costruttore
 
     public Percezione(Emozione emozione, int score, String note, String songId, String userId) {
         this.emozione = emozione;
@@ -16,21 +31,47 @@ public class Percezione {
         this.userId = userId;
     }
 
+    //getter
+    /**
+     * Restituisce l'emozione provata rappresentata dall'oggetto che esegue il metodo
+     * @return l'emozione selezionata
+     */
+
     public Emozione getEmozione() {
         return emozione;
     }
+
+    /**
+     * Restituisce il punteggio di intensità dell'emozione provata rappresentata dall'oggetto che esegue il metodo
+     * @return il punteggio di intensità dell'emozione
+     */
 
     public int getScore() {
         return score;
     }
 
+    /**
+     * Restituisce lo spazio di testo per inserire dettagli sull'emozione rappresentata dall'oggetto che esegue il metodo
+     * @return lo spazio di testo
+     */
+
     public String getNote() {
         return note;
     }
 
+    /**
+     * Restituisce il nome della canzone associata all'emozione rappresentata dall'oggetto che esegue il metodo
+     * @return il nome della canzone
+     */
+
     public String getSongId() {
         return songId;
     }
+
+    /**
+     * Restituisce il nome dell'utente che ha selezionato l'emozione rappresentata dall'oggetto che esegue il metodo
+     * @return il nome dell'utente
+     */
 
     public String getUserId() {
         return userId;
